24f751cb644f19b805696d8edafb4f11462946ed
Wanderley Caloni <wanderley.caloni@gmail.com>
2023-02-14 15:03:08 -0300

    Implement reverse string question.

--- reverse_string.cpp
+++ reverse_string.cpp
@@ -7,6 +7,19 @@ using namespace std;
 
 void ReverseString()
 {
+    string s = "this is the normal string to be reversed";
+    cout << s << endl;
+    cout << "reversing\n";
+    size_t beg = 0, end = s.size() - 1;
+    while (beg < end)
+    {
+        char buf = s[beg];
+        s[beg] = s[end];
+        s[end] = buf;
+        ++beg;
+        --end;
+    }
+    cout << s << endl;
 }
 
 ADD_INTERVIEW_QUESTION(ReverseString);
