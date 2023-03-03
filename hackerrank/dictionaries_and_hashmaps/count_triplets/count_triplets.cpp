cefd966c8d4eaca8fbda1d48c32f0a248cec99ff
Wanderley Caloni <wanderley.caloni@gmail.com>
2023-03-03 18:25:45 -0200

    Fix the timeout bugs in gh-3 (1h).
    After confabulate a lot with ChatGPT this is the best response we came up
    with. I found the left/right solution genius, but it did not solved the
    ratio equals 1, so I had to use my old solution for this case.

--- hackerrank/dictionaries_and_hashmaps/count_triplets/count_triplets.cpp
+++ hackerrank/dictionaries_and_hashmaps/count_triplets/count_triplets.cpp
@@ -17,55 +17,54 @@ long long nChoosek(long n, long k)
     for (long i = 2; i <= k; ++i) {
         result *= (n - i + 1);
         result /= i;
+
     }
     return result;
 }
 
-// Complete the countTriplets function below.
+/* Complete the countTriplets function below.
+
+ChatGPT helped me with that. In its words:
+
+This problem is known as the "Count Triplets" problem.
+It is a popular problem in coding interviews and is often used to test a
+candidate's ability to come up with efficient algorithms for
+processing arrays.
+
+The approach I showed you is known as the "hashmap" or "map" approach, and
+it's based on the observation that a valid triplet (i, j, k) must have the
+property that arr[j] = arr[i] * r and arr[k] = arr[j] * r = arr[i] * r^2.
+By keeping track of the frequency of elements that come before and after each
+element in the array, we can easily find the number of valid triplets in O(n)
+time complexity.
+*/
 long long countTriplets(vector<long> arr, long r) {
     long long triplets = 0;
-    map<long, vector<int>> indexes;
+    map<long, long> left, right;
 
-    // get the index for all numbers
+    // count all elements 'at the right' before we started
     for (int i = 0; i < arr.size(); ++i) {
-        indexes[arr[i]].push_back(i);
+        right[arr[i]]++;
     }
 
-    // check directly in the indexes
-    for (const auto& item : indexes) {
-        const vector<int>& firsts = item.second;
-        const auto& seconds = indexes.find(item.first * r);
-        const auto& thirds = indexes.find(item.first * r * r);
-
-        // the seconds and thirds are the same; apply combination formula
-        if (r == 1) {
-            triplets = nChoosek(firsts.size(), 3);
+    // count all possible triplets with i as the middle element
+    if (r == 1) {
+        for (const auto& element : right) {
+            triplets += nChoosek(element.second, 3);
         }
-        // we have seconds and thirds
-        else if (seconds != indexes.end() && thirds != indexes.end()) {
+        return triplets;
+    }
 
-            // for every first item of triplet
-            for (int firstIdx = 0; firstIdx < (int)firsts.size(); ++firstIdx) {
-                int firstPos = firsts[firstIdx];
+    // now we traverse the list again counting the left
+    // and calculating how many are in the middle term
+    for (int i = 0; i < arr.size(); ++i) {
+        right[arr[i]]--; // decrease from the 'at the right' list
 
-                // for every second item after the first
-                for (int secondIdx = 0; secondIdx < seconds->second.size(); ++secondIdx) {
-
-                    if (seconds->second[secondIdx] > firstPos) {
-                        int secondPos = seconds->second[secondIdx];
-
-                        // for every third item after the second
-                        for (int thirdIdx = 0; thirdIdx < thirds->second.size(); ++thirdIdx) {
-
-                            if (thirds->second[thirdIdx] > secondPos) {
-                                triplets += thirds->second.size() - thirdIdx;
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
+        if (arr[i] % r == 0) { // the middle term is divisible by the ratio
+            triplets += left[arr[i] / r] * right[arr[i] * r];
         }
+
+        left[arr[i]]++; // increase from the 'at the left' list
     }
 
     return triplets;
